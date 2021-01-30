fc9976eafb0894240d1f921ee6f46ded7647fc7c
Taylor Maxfield <taylor.p.maxfield@gmail.com>
2021-01-30 11:32:04 -0700

    Change runserver port to production

--- runserver.py
+++ runserver.py
@@ -6,9 +6,9 @@ from os import environ
 from Flask_Capstone import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = "0.0.0.0"
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('PORT', '5000'))
     except ValueError:
-        PORT = 5555
+        PORT = 5000
     app.run(HOST, PORT)
